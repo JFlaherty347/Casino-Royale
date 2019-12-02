54a9dd87254cffe7eec051daefd4df3b1a38f71c
JFlaherty347 <JFlaherty347@gmail.com>
2019-12-01 19:59:59 -0800

    Removed unecessary enviornment files

--- liars dice/bid.py
+++
@@ -1,9 +0,0 @@
-class bid:
-	#Face- the resulting number on the die (1-6)
-	#quantity- predicted count of said face (max 15 with 3 players all rolling the same number on all of their dice)
-	def __init__(self, face, quantity):
-		self.face = face
-		self.quantity = quantity
-
-	def toString(self):
-		return "BID: more than %d %ds\n"  %(self.quantity, self.face)
