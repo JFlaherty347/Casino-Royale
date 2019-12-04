6cd0eee31684974f4ce4017883d893e092ff1345
JFlaherty347 <JFlaherty347@gmail.com>
2019-12-04 13:32:32 -0800

    Removed unused resources

--- resources/bond.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.mixer.music.load('bond.mp3')
-pygame.mixer.music.play(1)
-pygame.mixer.music.set_volume(0.4)
-
-while pygame.mixer.music.get_busy():
-	pygame.event.poll()
